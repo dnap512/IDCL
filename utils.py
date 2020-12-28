10f981693e2f83822aca6badda224bcb8141a7cd
dnap512 <frshield3@gmail.com>
2020-12-28 13:33:12 +0900

    PyTorch implementation codes of IDCL

---
+++ utils.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+import torch
+import torchvision.transforms as transforms
+
+from torchvision import models
+from torchvision.datasets import ImageFolder, DatasetFolder
+import torchvision.datasets as Datasets
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+from torch import nn, optim
+from collections import OrderedDict
+
+import os
+import copy
+
+# +
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from torch import nn
+from torch.nn import functional as F
+
+# -
+
+def plotting(losses, accuracies, used_model, save_dir, is_pretrained, try_check):
+    plt.figure(figsize=(8,2))
+    plt.subplots_adjust(wspace=0.2)
+
+    plt.subplot(1,2,1)
+    plt.title("$Accuracy$", fontsize = 18)
+    plt.plot(accuracies)
+    plt.grid()
+    plt.xlabel("$epochs$", fontsize = 16)
+    plt.xticks(fontsize = 14)
+    plt.yticks(fontsize = 14)
+    
+    plt.subplot(1,2,2)
+    plt.title("$Loss$",fontsize = 18)
+    plt.plot(losses)
+    plt.grid()
+    plt.xlabel("$epochs$", fontsize = 16)
+    plt.xticks(fontsize = 14)
+    plt.yticks(fontsize = 14)
+
+    if is_pretrained:
+        plt.savefig(save_dir+'transfer_training_{}_{}.png'.format(used_model,str(try_check).zfill(2)), dpi=300)
+    else:
+        plt.savefig(save_dir+'scratch_training_{}_{}.png'.format(used_model,str(try_check).zfill(2)), dpi=300)
+    plt.show()
+
+
+def save_model(model, used_model, save_dir, is_pretrained, try_check):
+    if is_pretrained:
+        torch.save(model.state_dict(), 
+                   save_dir+'transfer_{}_{}.pth'.format(used_model,str(try_check).zfill(2)))
+    else:
+        torch.save(model.state_dict(),
+                   save_dir+'scratch_{}_{}.pth'.format(used_model,str(try_check).zfill(2)))
+
+
+def do_test(device, model,criterion, test_loader,used_model, save_dir, try_check):
+    batch = 0
+    test_accuracy = 0
+    
+    f = open(save_dir+'test_{}_{}_log.txt'.format(used_model,str(try_check).zfill(2)),'w')
+    
+    #for multi-class accuracy
+    y_pred=[]
+    y_true=[]
+
+    model.eval()
+    for idx, (x, y) in enumerate(test_loader):
+        x = x.to(device)
+        y = y.to(device)
+
+        output = model(x)
+        loss = criterion(output,y)
+
+        
+        _, preds = torch.max(output.data, 1)
+        accuracy = torch.sum(preds == y.data)
+        
+        #for multi-class accuracy
+        y_pred.append(preds)
+        y_true.append(y.data)
+
+        test_accuracy += accuracy.item()
+        batch += len(x)
+
+        log = 'Test:  [{}/{} ({:.0f}%)],\tAccuracy: {:.1f}%,  \t Loss: {:.6f}'.format(
+            batch, len(test_loader.dataset), 100.*(idx+1)/len(test_loader), 100.*(test_accuracy/batch), loss.item())
+        print(log)
+        f.write(log+'\n')
+    f.close()
+    
+    return str(round(test_accuracy/batch,4)), y_pred, y_true
+
+
+def do_training(device, epochs, model,optimizer, criterion, 
+                train_loader_stage1,train_loader_stage2, train_loader_stage3,
+                val_loader_stage1, val_loader_stage2, val_loader_stage3,
+                lr_scheduler
+               ):
+    val_losses = list()
+    val_accuracies = list()
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    avg_loss_val = 0
+    avg_acc_val = 0
+    
+
+    epoch_1 = (epochs//3)//2
+    epoch_2 = epochs//3
+    
+
+    for epoch in range(epochs):
+        epoch_loss = 0
+        epoch_accuracy = 0
+        batch = 0
+        
+        loss_val=0
+        acc_val=0
+        
+        model.train(True)
+        
+        if epoch < epoch_1:
+            train_loader = train_loader_stage1
+            val_loader = val_loader_stage1
+        elif epoch_1 <= epoch and epoch < epoch_2:
+            train_loader = train_loader_stage2
+            val_loader = val_loader_stage2
+        else:
+            train_loader = train_loader_stage3
+            val_loader = val_loader_stage3
+        
+        val_set_size = len(val_loader.dataset)
+        val_batches = len(val_loader)  
+        
+        for idx, (x,  y) in enumerate(train_loader):
+            
+            x = x.to(device)
+            y = y.to(device)
+            
+            
+            optimizer.zero_grad()
+            
+            output = model(x)
+            loss = criterion(output,y)
+            
+            loss.backward()
+            optimizer.step()
+            
+            _, preds = torch.max(output, 1)
+            accuracy = torch.sum(preds == y.data)
+
+            epoch_accuracy += accuracy.item()
+            epoch_loss += loss.item()
+
+            batch += len(x)
+            print('Epoch: {} [{}/{} ({:.0f}%)],\tAccuracy: {:.1f}%,  \t Loss: {:.6f}'.format(
+                epoch+1, batch, len(train_loader.dataset),100. * (idx+1) / len(train_loader), 100.*(accuracy.item()/len(x)), loss.item()))
+
+        lr_scheduler.step()
+        torch.cuda.empty_cache()
+         
+        
+        ###################################
+        # Validation
+        ###################################
+        
+        model.train(False)
+        model.eval()
+        
+        with torch.no_grad():
+            for i, (x, y) in enumerate(val_loader):
+                x = x.to(device)
+                y = y.to(device)
+
+                optimizer.zero_grad()
+                
+                output = model(x)
+                loss = criterion(output,y)
+                _, preds = torch.max(output.data, 1)
+
+                loss_val += loss.data
+                acc_val += torch.sum(preds == y.data)
+        
+            torch.cuda.empty_cache()
+        
+               
+        avg_loss_val = loss_val / val_set_size
+        avg_acc_val = acc_val.item() / val_set_size
+        
+        val_losses.append(avg_loss_val)       
+        val_accuracies.append(avg_acc_val)
+        
+        print("loss_val:",loss_val.item(),val_set_size)
+        print("acc_val:",acc_val.item(),val_set_size)
+        
+        print("Validation","="*50)
+        print("Avg loss (val): {:.4f}".format(avg_loss_val))
+        print("Avg acc (val): {:.4f}".format(avg_acc_val))
+        
+        if epoch >= epoch_2 and avg_acc_val > best_acc:
+            print("Best Weight is Updated!")
+            best_acc = avg_acc_val
+            best_model_wts = copy.deepcopy(model.state_dict())
+    
+
+    print("Best validation acc: {:.4f}".format(best_acc))    
+    model.load_state_dict(best_model_wts)
+            
+    return model, val_losses, val_accuracies
+
+
+def save_route(test_idx, domains, dataset, save_name, used_model):
+    dom = ''
+    for i in range(4):
+        if i==test_idx:
+            continue
+        dom += domains[i]
+        dom += '+'
+    save_dir = os.path.join(used_model,dataset)+'/{}/'.format(save_name)+dom[:-1]+'('+domains[test_idx]+')/'
+    return save_dir
+
+
+# +
+def get_tf(color_jitter=False, augment=True):
+    train_tf = 0
+    if color_jitter:
+        train_tf = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(.4, .4, .4, .5),
+            transforms.RandomGrayscale(p=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    else:
+        train_tf = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    test_tf = transforms.Compose([
+                    transforms.Resize((224, 224)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ])
+    if augment==False:
+        train_tf = test_tf
+    return train_tf, test_tf
+
+
+
+
+# -
+
+def save_model_setting(settings,used_model,domains, dataset, save_name):
+    text_file_name="model_parameter_setting_info.txt"
+    save_dir = os.path.join(used_model,dataset,save_name)
+        
+    try:
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+    except:
+        print('Error : Creating directory. '+ save_dir)
+        
+    save_dir=os.path.join(save_dir,text_file_name)
+    
+    with open(save_dir,"w") as f:
+        for key,value in settings.items():
+            f.write(key+" : "+str(value)+"\n") 
+
